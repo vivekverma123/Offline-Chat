74c942cf97b0dac6009d6382ee97559501a6e72d
Vivek <123vivekverma@gmail.com>
2020-10-01 12:46:33 +0530

    Commit No: 1 | Basic App Created

---
+++ app/src/main/java/com/example/offlinechat/ChatUtils.java
@@ -0,0 +1,320 @@
+package com.example.offlinechat;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class ChatUtils {
+    private static final String APP_NAME = "BluetoothChatApp";
+    private static final UUID MY_UUID = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private final BluetoothAdapter bluetoothAdapter;
+    private final Handler handler;
+    private AcceptThread acceptThread;
+    private ConnectThread connectThread;
+    private ReadWriteThread connectedThread;
+    private int state;
+
+    static final int STATE_NONE = 0;
+    static final int STATE_LISTEN = 1;
+    static final int STATE_CONNECTING = 2;
+    static final int STATE_CONNECTED = 3;
+
+    public ChatUtils(Context context, Handler handler) {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        state = STATE_NONE;
+
+        this.handler = handler;
+    }
+
+    private synchronized void setState(int state) {
+        this.state = state;
+
+        handler.obtainMessage(MainActivity.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
+    }
+
+    public synchronized int getState() {
+        return state;
+    }
+
+    public synchronized void start() {
+      
+        if (connectThread != null) {
+            connectThread.cancel();
+            connectThread = null;
+        }
+
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        setState(STATE_LISTEN);
+        if (acceptThread == null) {
+            acceptThread = new AcceptThread();
+            acceptThread.start();
+        }
+    }
+
+    public synchronized void connect(BluetoothDevice device) {
+      
+        if (state == STATE_CONNECTING) {
+            if (connectThread != null) {
+                connectThread.cancel();
+                connectThread = null;
+            }
+        }
+
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        connectThread = new ConnectThread(device);
+        connectThread.start();
+        setState(STATE_CONNECTING);
+    }
+
+    public synchronized void connected(BluetoothSocket socket, BluetoothDevice device) {
+       
+        if (connectThread != null) {
+            connectThread.cancel();
+            connectThread = null;
+        }
+
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        if (acceptThread != null) {
+            acceptThread.cancel();
+            acceptThread = null;
+        }
+
+        connectedThread = new ReadWriteThread(socket);
+        connectedThread.start();
+
+        Message msg = handler.obtainMessage(MainActivity.MESSAGE_DEVICE_OBJECT);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(MainActivity.DEVICE_OBJECT, device);
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+
+        setState(STATE_CONNECTED);
+    }
+    
+    public synchronized void stop() {
+        if (connectThread != null) {
+            connectThread.cancel();
+            connectThread = null;
+        }
+
+        if (connectedThread != null) {
+            connectedThread.cancel();
+            connectedThread = null;
+        }
+
+        if (acceptThread != null) {
+            acceptThread.cancel();
+            acceptThread = null;
+        }
+        setState(STATE_NONE);
+    }
+
+    public void write(byte[] out) {
+        ReadWriteThread r;
+        synchronized (this) {
+            if (state != STATE_CONNECTED)
+                return;
+            r = connectedThread;
+        }
+        r.write(out);
+    }
+
+    private void connectionFailed() {
+        Message msg = handler.obtainMessage(MainActivity.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString("toast", "Unable to connect device");
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+
+        ChatUtils.this.start();
+    }
+
+    private void connectionLost() {
+        Message msg = handler.obtainMessage(MainActivity.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString("toast", "Device connection was lost");
+        msg.setData(bundle);
+        handler.sendMessage(msg);
+
+        ChatUtils.this.start();
+    }
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket serverSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tmp = null;
+            try {
+                tmp = bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(APP_NAME, MY_UUID);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            serverSocket = tmp;
+        }
+
+        public void run() {
+            setName("AcceptThread");
+            BluetoothSocket socket;
+            while (state != STATE_CONNECTED) {
+                try {
+                    socket = serverSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+
+                if (socket != null) {
+                    synchronized (ChatUtils.this) {
+                        switch (state) {
+                            case STATE_LISTEN:
+                            case STATE_CONNECTING:
+                                
+                                connected(socket, socket.getRemoteDevice());
+                                break;
+                            case STATE_NONE:
+                            case STATE_CONNECTED:
+                              
+                                try {
+                                    socket.close();
+                                } catch (IOException e) {
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void cancel() {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket socket;
+        private final BluetoothDevice device;
+
+        public ConnectThread(BluetoothDevice device) {
+            this.device = device;
+            BluetoothSocket tmp = null;
+            try {
+                tmp = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            socket = tmp;
+        }
+
+        public void run() {
+            setName("ConnectThread");
+
+            bluetoothAdapter.cancelDiscovery();
+
+            try {
+                socket.connect();
+            } catch (IOException e) {
+                try {
+                    socket.close();
+                } catch (IOException e2) {
+                }
+                connectionFailed();
+                return;
+            }
+
+            synchronized (ChatUtils.this) {
+                connectThread = null;
+            }
+
+            connected(socket, device);
+        }
+
+        public void cancel() {
+            try {
+                socket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private class ReadWriteThread extends Thread {
+        private final BluetoothSocket bluetoothSocket;
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+
+        public ReadWriteThread(BluetoothSocket socket) {
+            this.bluetoothSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+            }
+
+            inputStream = tmpIn;
+            outputStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                   
+                    bytes = inputStream.read(buffer);
+
+                  
+                    handler.obtainMessage(MainActivity.MESSAGE_READ, bytes, -1,
+                            buffer).sendToTarget();
+                } catch (IOException e) {
+                    connectionLost();
+                    ChatUtils.this.start();
+                    break;
+                }
+            }
+        }
+
+        public void write(byte[] buffer) {
+            try {
+                outputStream.write(buffer);
+                handler.obtainMessage(MainActivity.MESSAGE_WRITE, -1, -1,buffer).sendToTarget();
+            } catch (IOException e) {
+            }
+        }
+
+        public void cancel() {
+            try {
+                bluetoothSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/offlinechat/MainActivity.java
@@ -0,0 +1,296 @@
+package com.example.offlinechat;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView status;
+    private Button btnConnect;
+    private ListView listView;
+    private Dialog dialog;
+    private TextInputLayout inputLayout;
+    private ArrayAdapter<String> chatAdapter;
+    private ArrayList<String> chatMessages;
+    private BluetoothAdapter bluetoothAdapter;
+
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_OBJECT = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final String DEVICE_OBJECT = "device_name";
+
+    private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+    private ChatUtils chatController;
+    private BluetoothDevice connectingDevice;
+    private ArrayAdapter<String> discoveredDevicesAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        findViewsByIds();
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPrinterPickDialog();
+            }
+        });
+
+        chatMessages = new ArrayList<>();
+        chatAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chatMessages);
+        listView.setAdapter(chatAdapter);
+    }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case ChatUtils.STATE_CONNECTED:
+                            setStatus("Connected to: " + connectingDevice.getName());
+                            btnConnect.setEnabled(false);
+                            break;
+                        case ChatUtils.STATE_CONNECTING:
+                            setStatus("Connecting...");
+                            btnConnect.setEnabled(false);
+                            break;
+                        case ChatUtils.STATE_LISTEN:
+                        case ChatUtils.STATE_NONE:
+                            setStatus("Not connected");
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+
+                    String writeMessage = new String(writeBuf);
+                    chatMessages.add("Me: " + writeMessage);
+                    chatAdapter.notifyDataSetChanged();
+                    break;
+                case MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    chatMessages.add(connectingDevice.getName() + ":  " + readMessage);
+                    chatAdapter.notifyDataSetChanged();
+                    break;
+                case MESSAGE_DEVICE_OBJECT:
+                    connectingDevice = msg.getData().getParcelable(DEVICE_OBJECT);
+                    Toast.makeText(getApplicationContext(), "Connected to " + connectingDevice.getName(),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(), msg.getData().getString("toast"),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        }
+    });
+
+    private void showPrinterPickDialog() {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.layout_bluetooth);
+        dialog.setTitle("Bluetooth Devices");
+
+        if (bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+        }
+        bluetoothAdapter.startDiscovery();
+
+        ArrayAdapter<String> pairedDevicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        discoveredDevicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        ListView listView = (ListView) dialog.findViewById(R.id.pairedDeviceList);
+        ListView listView2 = (ListView) dialog.findViewById(R.id.discoveredDeviceList);
+        listView.setAdapter(pairedDevicesAdapter);
+        listView2.setAdapter(discoveredDevicesAdapter);
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(discoveryFinishReceiver, filter);
+
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(discoveryFinishReceiver, filter);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDevicesAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            pairedDevicesAdapter.add(getString(R.string.none_paired));
+        }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                bluetoothAdapter.cancelDiscovery();
+                String info = ((TextView) view).getText().toString();
+                String address = info.substring(info.length() - 17);
+
+                connectToDevice(address);
+                dialog.dismiss();
+            }
+
+        });
+
+        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                bluetoothAdapter.cancelDiscovery();
+                String info = ((TextView) view).getText().toString();
+                String address = info.substring(info.length() - 17);
+
+                connectToDevice(address);
+                dialog.dismiss();
+            }
+        });
+
+        dialog.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.setCancelable(false);
+        dialog.show();
+    }
+
+    private void setStatus(String s) {
+        status.setText(s);
+    }
+
+    private void connectToDevice(String deviceAddress) {
+        bluetoothAdapter.cancelDiscovery();
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+        chatController.connect(device);
+    }
+
+    private void findViewsByIds() {
+        status = (TextView) findViewById(R.id.status);
+        btnConnect = (Button) findViewById(R.id.btn_connect);
+        listView = (ListView) findViewById(R.id.list);
+        inputLayout = (TextInputLayout) findViewById(R.id.input_layout);
+        View btnSend = findViewById(R.id.btn_send);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (inputLayout.getEditText().getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Please input some texts", Toast.LENGTH_SHORT).show();
+                } else {
+                    sendMessage(inputLayout.getEditText().getText().toString());
+                    inputLayout.getEditText().setText("");
+                }
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BLUETOOTH:
+                if (resultCode == Activity.RESULT_OK) {
+                    chatController = new ChatUtils(this, handler);
+                } else {
+                    Toast.makeText(this, "Bluetooth still disabled, turn off application!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    private void sendMessage(String message) {
+        if (chatController.getState() != ChatUtils.STATE_CONNECTED) {
+            Toast.makeText(this, "Connection was lost!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (message.length() > 0) {
+            byte[] send = message.getBytes();
+            chatController.write(send);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+        } else {
+            chatController = new ChatUtils(this, handler);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (chatController != null) {
+            if (chatController.getState() == ChatUtils.STATE_NONE) {
+                chatController.start();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (chatController != null)
+            chatController.stop();
+    }
+
+    private final BroadcastReceiver discoveryFinishReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    discoveredDevicesAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                if (discoveredDevicesAdapter.getCount() == 0) {
+                    discoveredDevicesAdapter.add(getString(R.string.none_found));
+                }
+            }
+        }
+    };
+}
